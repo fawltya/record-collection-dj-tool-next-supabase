f32aa723a2e880bfc009517b2bb25927eff18d9f
Seamus Barnett <seamus0689@gmail.com>
2024-01-21 14:37:08 +0000

    Sidebar added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,14 @@
+import Sidebar from "@/components/ui/sidebar";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+
+// export default async function Page() {
+//   return (
+//     <>
+//       <Sidebar />
+//     </>
+//   );
+// }
 
 export default async function Page() {
   const cookieStore = cookies();
@@ -8,5 +17,9 @@ export default async function Page() {
     .from("record_collection")
     .select();
 
-  return <pre>{JSON.stringify(recordCollection, null, 2)}</pre>;
+    return (
+        <>
+        </>
+        <pre>{JSON.stringify(recordCollection, null, 2)}</pre>)
+    
 }
