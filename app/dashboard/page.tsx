7f92b2bd0512040b4bdb300451719cf6bbc68353
Seamus Barnett <seamus0689@gmail.com>
2024-01-21 12:55:07 +0000

    Supabase connection working

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: recordCollection } = await supabase
+    .from("record_collection")
+    .select();
+
+  return <pre>{JSON.stringify(recordCollection, null, 2)}</pre>;
+}
